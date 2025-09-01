e3a3700ba30ff7a04ed42027bfbd323b9356823e
techguy16 <88870951+techguy16@users.noreply.github.com>
2025-09-02 08:47:05 +1200

    Something broke

--- scriptlets/Force yad to run under Xwayland.sh
+++ scriptlets/Force yad to run under Xwayland.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 script() {
-    echo "alias yad='GDK_BACKEND=x11 yad'" >> $HOME/.bashrc
+    echo "/home/$1/.bashrc"
+    echo "alias yad='GDK_BACKEND=x11 yad'" >> "/home/$1/.bashrc"
 }
 
-script
+script "$1"
